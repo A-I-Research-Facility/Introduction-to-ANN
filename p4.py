8218339c12185966b15c62c57df0d7d24b6621de
Home Residence <homeresidence@Homes-iMac.local>
2021-04-06 18:05:54 +0530

    Intro to dense layer

--- p4.py
+++ p4.py
@@ -1,12 +1,24 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
+np.random.seed(0)
 
-weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91,
-                                   0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-biases = [2, 3, 0.5]
 
-output = np.dot(weights, inputs) + biases
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-print(output)
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+#print(layer1.output)
+
+layer2.forward(layer1.output)
+print(layer2.output)
