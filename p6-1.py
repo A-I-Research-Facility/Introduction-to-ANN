4c8991fe0235e335d89cab06ca4c9f076f6c51a2
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-04-06 22:00:47 +0530

    update

---
+++ p6-1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import nnfs
+
+nnfs.init()
+
+layer_outputs = [[4.8, 1.21, 2.385],
+                 [8.9, -1.81, 0.2],
+                 [1.41, 1.051, 0.026]]
+
+exp_values = np.exp(layer_outputs)
+
+norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+print(norm_values)
+# print(sum(norm_values))
